1b0fdc9526784a35f3f46379fbf976c59d506228
Julia Aasmaa <my_e_10@hotmail.com>
2021-09-14 12:05:45 +0300

    Add OR and AND

---
+++ BooleanLogicalOperators/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace BooleanLogicalOperators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus;
+            //programm küsib kasutajal sisestada parool;
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus
+            //kui ka parool on õiged;
+            //kui mõlemad, kasutajatunnus ja parool on õiged
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui kasutajatunnus või parool on vale, siis 
+            //programm kuvab "sisselogimine ebaõnnestus. Proovi uuesti."
+            //kt: admin, pr:admin1234
+
+            Console.WriteLine("Sisesta kasutajatunnus");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool:");
+            string userPassword = Console.ReadLine();
+
+            if(userName == "admin" && userPassword == "admin1234")
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+            else
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus.Proovi uuesti.");
+            }
+
+            
+        }
+    }
+}
---
+++ Login/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Login
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada kasutajatunnus;
+            //programm küsib kasutajal sisestada parool;
+            //programm kontrollib, kas mõlemad, nii kasutajatunnus
+            //kui ka parool on õiged;
+            //kui mõlemad, kasutajatunnus ja parool on õiged
+            //programm kuvab konsoolis "Tere tulemast!"
+            //kui kasutajatunnus või parool on vale, siis 
+            //programm kuvab "sisselogimine ebaõnnestus. Proovi uuesti."
+            //kt: admin, pr:admin1234
+
+            Console.WriteLine("Sisesta kasutajatunnus");
+            string userName = Console.ReadLine();
+            Console.WriteLine("Sisesta parool:");
+            string userPassword = Console.ReadLine();
+
+            if (userName != "admin" || userPassword != "admin1234")
+            {
+                Console.WriteLine("Sisselogimine ebaõnnestus.Proovi uuesti.");
+            }
+            else
+            {
+                Console.WriteLine("Tere tulemast!");
+            }
+
+        }
+    }
+}
